def5763cb39bf748d105864e91907d3da3c272d9
kongsanais <kongsanais@gmail.com>
2021-07-28 14:00:28 +0700

    commit code

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController, LoadingController, NavController, ToastController } from '@ionic/angular';
@@ -11,21 +11,68 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginPage implements OnInit {
   registerForm: FormGroup;
+  loginForm : FormGroup;
+
+
+  @ViewChild('flipcontainer', { static: false }) flipcontainer: ElementRef;
 
   constructor(private fb: FormBuilder, private authService: AuthService, private loadingCtrl: LoadingController,
     private toastCtrl: ToastController, private alertCtrl: AlertController, private router: Router) { }
    
   ngOnInit() {
+    
+    /* register form */
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       name: ['', Validators.required],
       role: ['BUYER', Validators.required]
     });
+
+    /* login form */ 
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
   }
 
 
+  navigateByRole(role){
+     if(role == 'BUYER'){
+
+     }else if(role == "SELLER"){
+
+     }
+  }
+
+
+  async login() {
+    let loading = await this.loadingCtrl.create({
+      message: 'Loading...'
+    });
+    await loading.present();
+    this.authService.signIn(this.loginForm.value).subscribe(user => {
+      loading.dismiss();
+      console.log('after login: ', user);
+      //this.navigateByRole(user['role']);
+    },
+    async err => {
+      loading.dismiss();
+
+      let alert = await this.alertCtrl.create({
+        header: 'Error',
+        message: err.message,
+        buttons: ['OK']
+      });
+      alert.present();
+    })
+  }
+
+
+
   async register(){
+
     let loading = await this.loadingCtrl.create({
       message : 'Loading...'
     })
@@ -34,10 +81,10 @@ export class LoginPage implements OnInit {
     this.authService.signUp(this.registerForm.value).then(async res => {
     await loading.dismiss();
 
-      let toast = await this.toastCtrl.create({
+    let toast = await this.toastCtrl.create({
         duration: 3000,
         message: 'Successfully created new Account!'
-      });
+    });
 
       toast.present();
       console.log('finished :', res);
@@ -52,6 +99,11 @@ export class LoginPage implements OnInit {
       });
       alert.present();
     });
+  }
+
+
+  toggleRegister() {
+    this.flipcontainer.nativeElement.classList.toggle('flip');
   }
 
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
+import { Observable, from, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { NavController } from '@ionic/angular';
-import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,23 @@ export class AuthService {
 
   constructor(private afAuth: AngularFireAuth, private db: AngularFirestore, private navCtrl: NavController) { }
 
-  signUp(credentials) {
 
+
+  signIn(credentials): Observable<any> {
+    return from(this.afAuth.signInWithEmailAndPassword(credentials.email, credentials.password)).pipe(
+      switchMap(user => {
+        console.log('real user: ', user);
+        if (user) {
+          return this.db.doc(`users/${user.user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    )
+  }
+
+  
+  signUp(credentials) {
     return this.afAuth.createUserWithEmailAndPassword(credentials.email, credentials.password).then(data => {
       console.log('after register: ', data);
       return this.db.doc(`users/${data.user.uid}`).set({
@@ -22,9 +39,10 @@ export class AuthService {
         created: firebase.default.firestore.FieldValue.serverTimestamp()
       });
     });
-
-    
   }
+
+
+
 
 
 }
